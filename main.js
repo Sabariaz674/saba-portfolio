5c077be1363b01549846caf9822df1e0b5553b43
Sabariaz674 <sabariaz593@gmail.com>
2025-08-21 09:55:57 -0700

    Added Node.js skill section in portfolio

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
   var typed = new Typed(".text", {
-    strings: ["FrontEnd Developer","custom Design","Rebuild Design",],
+    strings: ["FrontEnd Developer","Backened Developer","Full Stack Developer",],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
